8080064d61bc072c84728dea7e34595774aadf46
wendel.araujo <wendelca@gmail.com>
2021-12-21 10:24:49 -0300

    completo

--- src/main/java/com/example/accessingdata/MvcConfig.java
+++ src/main/java/com/example/accessingdata/MvcConfig.java
@@ -7,11 +7,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
-    registry.addViewController("/").setViewName("gerenciar-painel");
+    registry.addViewController("/home").setViewName("home");
+    registry.addViewController("/").setViewName("home");
+    registry.addViewController("/hello").setViewName("hello"); 
     registry.addViewController("/login").setViewName("login");
     registry.addViewController("/gerenciar-painel").setViewName("gerenciar-painel");
-    registry.addViewController("/painel").setViewName("painel");
-    registry.addViewController("/bootstrap.min.css").setViewName("bootstrap.min.css");
     registry.addViewController("/css/hrt.css").setViewName("css/hrt.css");
 	registry.addViewController("/img/Bandeira_do_Distrito_Federal_Brasil.jpg").setViewName
     ("img/Bandeira_do_Distrito_Federal_Brasil");
--- src/main/java/com/example/accessingdata/UserRepository.java
+++ src/main/java/com/example/accessingdata/UserRepository.java
@@ -1,16 +1,16 @@
 package com.example.accessingdata;
 
 
-import org.springframework.data.domain.Sort;
+//import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.CrudRepository;
 /**
  * UsuarioRepository
  */
-public interface UserRepository extends CrudRepository<User, Integer>{
+public interface UserRepository extends CrudRepository<User, Integer> {
 
     public User findById(int id);
     public User findByName(String name);
-    // public Iterable<User> findAll(Sort sort);
+   // public Iterable<User> findAll(Sort sort);
 
 }
 
